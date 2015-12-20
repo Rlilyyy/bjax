8031ff848553bbf8f7f30a389f3b7d2135707816
BingoLee <bingohlee@gmail.com>
2015-12-20 22:40:52 +0800

    添加事件跨浏览器支持

--- ToyBean.js
+++ ToyBean.js
@@ -7,6 +7,9 @@
 
 	var prevar = root.tb;
 
+	var addEventbean = addEventListener || attachEvent;
+	var removeEventbean = removeEventListener || detachEvent;
+
 	var tb = function(toy) {
 		switch(toy) {
 			case "selector":
@@ -35,8 +38,39 @@
 		return tb;
 	}
 	
+	// attachEvent的this指针是window，等待兼容
+	// tb.addEventToy(events, func, capturing) {
+	// 	if(addEventbean === attachEvent) {
+	// 		events = "on" + events;
+	// 	}
+	// 	addEventbean(events, func, capturing);
+	// }
 
-	
+	// tb.removeEventToy(events, func) {
+	// 	if(addEventbean === attachEvent) {
+	// 		events = "on" + events;
+	// 	}
+	// 	removeEventToy(events, func);
+	// }
+	tb.addHandler = function(elem, type, func) {
+		if(elem.addEventListener) {
+			elem.addEventListener(type, func, false);
+		} else if(elem.attachEvent) {
+			elem.attachEvent("on" + type, func);
+		} else {
+			elem["on" + type] = func;
+		}
+	}
+
+	tb.removeHandler = function(elem, type, func) {
+		if(elem.removeEventListener) {
+			elem.removeEventListener(type, func);
+		} else if(elem.detachEvent) {
+			elem.detachEvent("on" + type, func);
+		} else {
+			elem["on" + type] = null;
+		}
+	}
 
 
 })(this);
